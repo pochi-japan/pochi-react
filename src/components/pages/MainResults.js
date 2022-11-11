760ab8b81c84fef758b40b791e568ba89c50fe55
caterada <caterada@gmail.com>
2022-11-10 17:19:30 -0800

    get random pics to render on MainResults

--- src/components/pages/MainResults.js
+++ src/components/pages/MainResults.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
@@ -6,21 +6,41 @@ function MainResults(props) {
 	const [randomResults, setRandomResults] = useState([]);
 	const [error, setError] = useState(null);
 
-	// Read random results from API
+	// Function to randomize the results
+	function shuffle(array) {
+		for (let i = array.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			[array[i], array[j]] = [array[j], array[i]];
+		}
+	}
+
+	// Read results from API
 	useEffect(() => {
 		//update to heroku later
 		axios
 			.get(`http://localhost:8000/api`)
-			.then((res) => setRandomResults(res.data))
+			.then((res) => {
+				const data = shuffle(res.data);
+				//randomize the results here
+				setRandomResults(res.data);
+				console.log(res.data);
+			})
 			.catch((err) => {
 				setError(err.message);
 			});
-	});
+	}, []);
+
+	//Filter by category
+	// function filterPlace(randomResults, query) {
+	//     return randomResults.filter((el) => el.includes())
+	// }
+	// let placeResults = randomResults.filter(category => )
 
 	return (
 		<section>
 			Main Results - delete this line later
-			{randomResults.map((res) => {
+			{/* Limit random results to 5 images */}
+			{randomResults.slice(0, 3).map((res) => {
 				return (
 					<div key={`${res.name}-card`}>
 						<div className='results-img'>
