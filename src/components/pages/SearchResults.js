eb16a0e737bf35d7ff26c57f14b50487f1ab52ff
caterada <caterada@gmail.com>
2022-11-11 17:06:55 -0800

    resolve conflicts (2)

--- src/components/pages/SearchResults.js
+++ src/components/pages/SearchResults.js
@@ -47,32 +47,3 @@ function SearchResults({ searchString, setSearchString }) {
 }
 
 export default SearchResults;
-
-// 	return (
-// 		<section>
-// 			Main Results - delete this line later
-// 			{/* Limit random results to 5 images, set fallback image if image is null or gives an invalid image */}
-// 			{randomResults.slice(0, 4).map((res) => {
-// 				return (
-// 					<div key={`${res.name}-card`}>
-// 						<div className='results-img'>
-// 							<Link to={`/detail/:${res._id}`} key={res.name}>
-// 								<h1>{res.name}</h1>
-// 								<br />
-// 								<img
-// 									src={res.picture1}
-// 									onError={(e) =>
-// 										(e.currentTarget.src =
-// 											'https://media.giphy.com/media/qdFCb59rXKZ1K/giphy.gif')
-// 									}
-// 									alt={res.name}
-// 								/>
-// 							</Link>
-// 						</div>
-// 					</div>
-// 				);
-// 			})}
-// 			{error ? <div>{error}</div> : <div>{''}</div>}
-// 		</section>
-// 	);
-// }
